cd4d78551bdf15580df2a16ed0938ff443acd799
Karthik-Balli <karthikballi0001@gmail.com>
2024-11-16 11:16:39 +0530

    Create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ const path = useLocation();
             <FaMoon />
         </Button>
         <Link to="./sign-in">
-            <Button gradientDuoTone='purpleToBlue'>
+            <Button gradientDuoTone='purpleToBlue' outline>
                 SignIn
             </Button>
         </Link>

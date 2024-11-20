3d9806d920573b1d5bae5d39573bb969bb3eac11
Karthik-Balli <karthikballi0001@gmail.com>
2024-11-20 22:01:59 +0530

    Add redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,9 +2,6 @@ import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import {errorHandler } from '../utils/error.js'
 import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
-
-dotenv.config();
 
 
 export const signup = async (req, res, next) => {
@@ -41,11 +38,9 @@ export const signup = async (req, res, next) => {
   
   export const signin = async (req, res, next) => {
     const { email, password } = req.body;
-  
     if (!email || !password || email === '' || password === '') {
-      return next(errorHandler(400, 'All fields are required'));
+      next(errorHandler(400, 'All fields are required'));
     }
-  
     try {
       const validUser = await User.findOne({ email });
       if (!validUser) {
@@ -55,21 +50,15 @@ export const signup = async (req, res, next) => {
       if (!validPassword) {
         return next(errorHandler(400, 'Invalid password'));
       }
-
-      {/* 
       const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-  
       const { password: pass, ...rest } = validUser._doc;
-  
       res
         .status(200)
         .cookie('access_token', token, {
           httpOnly: true,
         })
         .json(rest);
-
-        */}
     } catch (error) {
-      return next(error);
+      next(error);
     }
   };

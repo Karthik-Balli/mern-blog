cb584297d91c95d5ecc2b2aee18a1d81bf9b4153
Karthik-Balli <karthikballi0001@gmail.com>
2025-01-31 21:49:41 +0530

    Created and started server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Dashboard from './pages/Dashboard'
 import SignIn from './pages/SignIn';
-import SignUp from './pages/Signup'
+import SignUp from './pages/SignUp'
 import Header from './components/Header'
 
 function App() {

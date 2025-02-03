c92980c21b2c1d8312b3fcf406ba4d1007afe684
Karthik-Balli <karthikballi0001@gmail.com>
2025-02-03 22:18:27 +0530

    Made the some components as private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Dashboard from './pages/Dashboard'
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/about" element={ <About /> } />
-        <Route path="/dashboard" element={ <Dashboard /> } />
-        <Route path="/signin" element={ <SignIn /> } />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
